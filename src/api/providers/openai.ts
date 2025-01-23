965f10647b24ef3db5f7403c155643957aa4bed2
sam hoang <samhv.ict@gmail.com>
2025-01-23 11:01:30 +0700

    Fix(openai) handle azure more correctly fix #430
    close #430

--- src/api/providers/openai.ts
+++ src/api/providers/openai.ts
@@ -17,8 +17,7 @@ export class OpenAiHandler implements ApiHandler, SingleCompletionHandler {
 	constructor(options: ApiHandlerOptions) {
 		this.options = options
 		// Azure API shape slightly differs from the core API shape: https://github.com/openai/openai-node?tab=readme-ov-file#microsoft-azure-openai
-		const urlHost = new URL(this.options.openAiBaseUrl ?? "").host
-		if (urlHost === "azure.com" || urlHost.endsWith(".azure.com")) {
+		if (this.options.openAiBaseUrl?.toLowerCase().includes("azure.com")) {
 			this.client = new AzureOpenAI({
 				baseURL: this.options.openAiBaseUrl,
 				apiKey: this.options.openAiApiKey,
