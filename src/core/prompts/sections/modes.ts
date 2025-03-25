60cca5e4dee2bb97218708579c9db7b1f56e2b1d
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2025-03-25 19:13:42 +0000

    Remove redundant code branch

--- src/core/prompts/sections/modes.ts
+++ src/core/prompts/sections/modes.ts
@@ -18,15 +18,12 @@ MODES
 - These are the currently available modes:
 ${allModes.map((mode: ModeConfig) => `  * "${mode.name}" mode (${mode.slug}) - ${mode.roleDefinition.split(".")[0]}`).join("\n")}`
 
-	// Only include custom modes documentation if the feature is enabled
-	if (true) {
-		modesContent += `
+	modesContent += `
 If the user asks you to create or edit a new mode for this project, you should read the instructions by using the fetch_instructions tool, like this:
 <fetch_instructions>
 <task>create_mode</task>
 </fetch_instructions>
 `
-	}
 
 	return modesContent
 }
