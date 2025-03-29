9b780d06cbd6f554597ad16986fa53f7a8b5625e
cte <cestreich@gmail.com>
2025-03-28 23:19:10 -0700

    More progress

--- src/exports/api.ts
+++ src/exports/api.ts
@@ -2,14 +2,17 @@ import { EventEmitter } from "events"
 import * as vscode from "vscode"
 
 import { ClineProvider } from "../core/webview/ClineProvider"
+import { openClineInNewTab } from "../activate/registerCommands"
 
-import { RooCodeAPI, RooCodeEvents, TokenUsage, RooCodeSettings, ClineMessage } from "./roo-code"
+import { TokenUsage, RooCodeSettings, RooCodeEvents, RooCodeEventName } from "../schemas"
+import { IpcOrigin, IpcMessageType, TaskCommandName, TaskEvent } from "../schemas/ipc"
+import { RooCodeAPI } from "./interface"
 import { MessageHistory } from "./message-history"
-import { IpcOrigin, IpcMessageType, IpcServer, TaskCommandName, TaskEventName } from "./ipc"
+import { IpcServer } from "./ipc"
 
 export class API extends EventEmitter<RooCodeEvents> implements RooCodeAPI {
 	private readonly outputChannel: vscode.OutputChannel
-	private readonly provider: ClineProvider
+	private provider: ClineProvider
 	private readonly history: MessageHistory
 	private readonly tokenUsage: Record<string, TokenUsage>
 	private readonly ipc?: IpcServer
@@ -23,19 +26,21 @@ export class API extends EventEmitter<RooCodeEvents> implements RooCodeAPI {
 		this.tokenUsage = {}
 
 		this.provider.on("clineCreated", (cline) => {
-			cline.on("message", (message) => this.emit("message", { taskId: cline.taskId, ...message }))
-			cline.on("taskStarted", () => this.emit("taskStarted", cline.taskId))
-			cline.on("taskPaused", () => this.emit("taskPaused", cline.taskId))
-			cline.on("taskUnpaused", () => this.emit("taskUnpaused", cline.taskId))
-			cline.on("taskAskResponded", () => this.emit("taskAskResponded", cline.taskId))
-			cline.on("taskAborted", () => this.emit("taskAborted", cline.taskId))
-			cline.on("taskSpawned", (childTaskId) => this.emit("taskSpawned", cline.taskId, childTaskId))
-			cline.on("taskCompleted", (_, usage) => this.emit("taskCompleted", cline.taskId, usage))
-			cline.on("taskTokenUsageUpdated", (_, usage) => this.emit("taskTokenUsageUpdated", cline.taskId, usage))
-			this.emit("taskCreated", cline.taskId)
+			cline.on("message", (message) => this.emit(RooCodeEventName.Message, { taskId: cline.taskId, ...message }))
+			cline.on("taskStarted", () => this.emit(RooCodeEventName.TaskStarted, cline.taskId))
+			cline.on("taskPaused", () => this.emit(RooCodeEventName.TaskPaused, cline.taskId))
+			cline.on("taskUnpaused", () => this.emit(RooCodeEventName.TaskUnpaused, cline.taskId))
+			cline.on("taskAskResponded", () => this.emit(RooCodeEventName.TaskAskResponded, cline.taskId))
+			cline.on("taskAborted", () => this.emit(RooCodeEventName.TaskAborted, cline.taskId))
+			cline.on("taskSpawned", (childTaskId) => this.emit(RooCodeEventName.TaskSpawned, cline.taskId, childTaskId))
+			cline.on("taskCompleted", (_, usage) => this.emit(RooCodeEventName.TaskCompleted, cline.taskId, usage))
+			cline.on("taskTokenUsageUpdated", (_, usage) =>
+				this.emit(RooCodeEventName.TaskTokenUsageUpdated, cline.taskId, usage),
+			)
+			this.emit(RooCodeEventName.TaskCreated, cline.taskId)
 		})
 
-		this.on("message", ({ taskId, action, message }) => {
+		this.on(RooCodeEventName.Message, ({ taskId, action, message }) => {
 			if (action === "created") {
 				this.history.add(taskId, message)
 			} else if (action === "updated") {
@@ -43,15 +48,104 @@ export class API extends EventEmitter<RooCodeEvents> implements RooCodeAPI {
 			}
 		})
 
-		this.on("taskTokenUsageUpdated", (taskId, usage) => (this.tokenUsage[taskId] = usage))
+		this.on(RooCodeEventName.TaskTokenUsageUpdated, (taskId, usage) => (this.tokenUsage[taskId] = usage))
 
 		if (socketPath) {
 			this.ipc = new IpcServer(socketPath)
 			this.ipc.listen()
+			this.outputChannel.appendLine(`IPC server started: ${socketPath}`)
 
-			this.ipc.on("taskCommand", (_clientId, command) => {
+			this.ipc.on("taskCommand", async (_clientId, command) => {
 				switch (command.commandName) {
+					case TaskCommandName.GetSettings:
+						this.getConfiguration()
+						break
+					case TaskCommandName.PutSettings:
+						this.setConfiguration(command.data)
+						break
 					case TaskCommandName.StartNewTask:
+						this.setConfiguration({
+							apiProvider: "openrouter",
+							openRouterModelId: "anthropic/claude-3.7-sonnet",
+							openRouterModelInfo: {
+								maxTokens: 8192,
+								contextWindow: 200000,
+								supportsImages: true,
+								supportsComputerUse: true,
+								supportsPromptCache: true,
+								inputPrice: 3,
+								outputPrice: 15,
+								cacheWritesPrice: 3.75,
+								cacheReadsPrice: 0.3,
+								description:
+									"Claude 3.7 Sonnet is an advanced large language model with improved reasoning, coding, and problem-solving capabilities. It introduces a hybrid reasoning approach, allowing users to choose between rapid responses and extended, step-by-step processing for complex tasks. The model demonstrates notable improvements in coding, particularly in front-end development and full-stack updates, and excels in agentic workflows, where it can autonomously navigate multi-step processes. \n\nClaude 3.7 Sonnet maintains performance parity with its predecessor in standard mode while offering an extended reasoning mode for enhanced accuracy in math, coding, and instruction-following tasks.\n\nRead more at the [blog post here](https://www.anthropic.com/news/claude-3-7-sonnet)",
+								thinking: false,
+							},
+
+							pinnedApiConfigs: {},
+							lastShownAnnouncementId: "mar-20-2025-3-10",
+
+							autoApprovalEnabled: true,
+							alwaysAllowReadOnly: true,
+							alwaysAllowReadOnlyOutsideWorkspace: false,
+							alwaysAllowWrite: true,
+							alwaysAllowWriteOutsideWorkspace: false,
+							writeDelayMs: 200,
+							alwaysAllowBrowser: true,
+							alwaysApproveResubmit: true,
+							requestDelaySeconds: 5,
+							alwaysAllowMcp: true,
+							alwaysAllowModeSwitch: true,
+							alwaysAllowSubtasks: true,
+							alwaysAllowExecute: true,
+							allowedCommands: ["*"],
+
+							browserToolEnabled: false,
+							browserViewportSize: "900x600",
+							screenshotQuality: 38,
+							remoteBrowserEnabled: true,
+
+							enableCheckpoints: false,
+							checkpointStorage: "task",
+
+							ttsEnabled: false,
+							ttsSpeed: 1,
+							soundEnabled: false,
+							soundVolume: 0.5,
+
+							maxOpenTabsContext: 20,
+							maxWorkspaceFiles: 200,
+							showRooIgnoredFiles: true,
+							maxReadFileLine: 500,
+
+							terminalOutputLineLimit: 500,
+							terminalShellIntegrationTimeout: 5000,
+
+							rateLimitSeconds: 0,
+							diffEnabled: true,
+							fuzzyMatchThreshold: 1.0,
+							experiments: {
+								experimentalDiffStrategy: false, // unified diff
+								multi_search_and_replace: false, // multi-line search and replace
+								search_and_replace: true, // single-line search and replace
+								insert_content: false,
+								powerSteering: false,
+							},
+
+							language: "en",
+
+							telemetrySetting: "enabled",
+
+							mcpEnabled: false,
+							mode: "code",
+							customModes: [],
+						})
+
+						this.provider = await openClineInNewTab({
+							context: this.provider.context,
+							outputChannel: this.outputChannel,
+						})
+
 						this.startNewTask(command.data.text, command.data.images)
 						break
 				}
@@ -63,120 +157,8 @@ export class API extends EventEmitter<RooCodeEvents> implements RooCodeAPI {
 		eventName: K,
 		...args: K extends keyof RooCodeEvents ? RooCodeEvents[K] : never
 	) {
-		if (this.ipc) {
-			switch (eventName) {
-				case TaskEventName.Message:
-					this.ipc.broadcast({
-						type: IpcMessageType.TaskEvent,
-						data: {
-							eventName: TaskEventName.Message,
-							data: args[0] as RooCodeEvents[TaskEventName.Message][0],
-						},
-						origin: IpcOrigin.Server,
-					})
-					break
-				case TaskEventName.TaskCreated:
-					this.ipc.broadcast({
-						type: IpcMessageType.TaskEvent,
-						data: {
-							eventName: TaskEventName.TaskCreated,
-							data: { taskId: args[0] as RooCodeEvents[TaskEventName.TaskCreated][0] },
-						},
-						origin: IpcOrigin.Server,
-					})
-					break
-				case TaskEventName.TaskStarted:
-					this.ipc.broadcast({
-						type: IpcMessageType.TaskEvent,
-						data: {
-							eventName: TaskEventName.TaskStarted,
-							data: { taskId: args[0] as RooCodeEvents[TaskEventName.TaskStarted][0] },
-						},
-						origin: IpcOrigin.Server,
-					})
-					break
-				case TaskEventName.TaskPaused:
-					this.ipc.broadcast({
-						type: IpcMessageType.TaskEvent,
-						data: {
-							eventName: TaskEventName.TaskPaused,
-							data: { taskId: args[0] as RooCodeEvents[TaskEventName.TaskPaused][0] },
-						},
-						origin: IpcOrigin.Server,
-					})
-					break
-				case TaskEventName.TaskUnpaused:
-					this.ipc.broadcast({
-						type: IpcMessageType.TaskEvent,
-						data: {
-							eventName: TaskEventName.TaskUnpaused,
-							data: { taskId: args[0] as RooCodeEvents[TaskEventName.TaskUnpaused][0] },
-						},
-						origin: IpcOrigin.Server,
-					})
-					break
-				case TaskEventName.TaskAskResponded:
-					this.ipc.broadcast({
-						type: IpcMessageType.TaskEvent,
-						data: {
-							eventName: TaskEventName.TaskAskResponded,
-							data: { taskId: args[0] as RooCodeEvents[TaskEventName.TaskAskResponded][0] },
-						},
-						origin: IpcOrigin.Server,
-					})
-					break
-				case TaskEventName.TaskAborted:
-					this.ipc.broadcast({
-						type: IpcMessageType.TaskEvent,
-						data: {
-							eventName: TaskEventName.TaskAborted,
-							data: { taskId: args[0] as RooCodeEvents[TaskEventName.TaskAborted][0] },
-						},
-						origin: IpcOrigin.Server,
-					})
-					break
-				case TaskEventName.TaskSpawned:
-					this.ipc.broadcast({
-						type: IpcMessageType.TaskEvent,
-						data: {
-							eventName: TaskEventName.TaskSpawned,
-							data: {
-								taskId: args[0] as RooCodeEvents[TaskEventName.TaskSpawned][0],
-								childTaskId: args[1] as RooCodeEvents[TaskEventName.TaskSpawned][1],
-							},
-						},
-						origin: IpcOrigin.Server,
-					})
-					break
-				case TaskEventName.TaskCompleted:
-					this.ipc.broadcast({
-						type: IpcMessageType.TaskEvent,
-						data: {
-							eventName: TaskEventName.TaskCompleted,
-							data: {
-								taskId: args[0] as RooCodeEvents[TaskEventName.TaskCompleted][0],
-								usage: args[1] as RooCodeEvents[TaskEventName.TaskCompleted][1],
-							},
-						},
-						origin: IpcOrigin.Server,
-					})
-					break
-				case TaskEventName.TaskTokenUsageUpdated:
-					this.ipc.broadcast({
-						type: IpcMessageType.TaskEvent,
-						data: {
-							eventName: TaskEventName.TaskTokenUsageUpdated,
-							data: {
-								taskId: args[0] as RooCodeEvents[TaskEventName.TaskTokenUsageUpdated][0],
-								usage: args[1] as RooCodeEvents[TaskEventName.TaskTokenUsageUpdated][1],
-							},
-						},
-						origin: IpcOrigin.Server,
-					})
-					break
-			}
-		}
-
+		const data = { eventName: eventName as RooCodeEventName, payload: args } as TaskEvent
+		this.ipc?.broadcast({ type: IpcMessageType.TaskEvent, origin: IpcOrigin.Server, data })
 		return super.emit(eventName, ...args)
 	}
 
