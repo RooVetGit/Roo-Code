326b647baf622c320526801719d62e53872bc82f
cte <cestreich@gmail.com>
2025-04-04 00:06:13 -0700

    More tweaks

--- evals/apps/cli/src/index.ts
+++ evals/apps/cli/src/index.ts
@@ -356,7 +356,7 @@ const runUnitTest = async ({ task }: { task: Task }) => {
 				break
 			}
 		} catch (error) {
-			console.log("[cli#run] execa error =", error)
+			console.log("[cli#runUnitTest] execa error =", error)
 			passed = false
 			break
 		}
