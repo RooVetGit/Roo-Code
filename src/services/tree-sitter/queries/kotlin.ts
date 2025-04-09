7cb7d32b8e8776edcca8407d060ee695f683620c
Eric Wheeler <roo-code@z.ewheeler.org>
2025-04-09 02:40:04 -0700

    feat: standardize Kotlin Tree-Sitter parser
    Standardized Kotlin Tree-Sitter parser to ensure consistent naming and structure:
    - Renamed identifiers to clearly indicate what they're testing
    - Ensured all code sections are at least 4 lines long
    - Organized query patterns logically with clear comments
    - Removed skipped tests and unused query patterns
    - Ensured only one test per structure or combination of structures
    
    Signed-off-by: Eric Wheeler <roo-code@z.ewheeler.org>

--- src/services/tree-sitter/queries/kotlin.ts
+++ src/services/tree-sitter/queries/kotlin.ts
@@ -1,28 +1,74 @@
 /*
-- class declarations (including interfaces)
-- function declarations
-- object declarations
-- property declarations
-- type alias declarations
+- class declarations (regular, data, abstract, sealed, enum, annotation)
+- interface declarations
+- function declarations (regular, suspend)
+- object declarations (including companion objects)
 */
 export default `
+; Regular class declarations
 (class_declaration
   (type_identifier) @name.definition.class
 ) @definition.class
 
+; Data class declarations
+(class_declaration
+  (modifiers
+    (class_modifier) @_modifier (#eq? @_modifier "data"))
+  (type_identifier) @name.definition.data_class
+) @definition.data_class
+
+; Abstract class declarations
+(class_declaration
+  (modifiers
+    (inheritance_modifier) @_modifier (#eq? @_modifier "abstract"))
+  (type_identifier) @name.definition.abstract_class
+) @definition.abstract_class
+
+; Sealed class declarations
+(class_declaration
+  (modifiers
+    (class_modifier) @_modifier (#eq? @_modifier "sealed"))
+  (type_identifier) @name.definition.sealed_class
+) @definition.sealed_class
+
+; Enum class declarations
+(class_declaration
+  (type_identifier)
+  (enum_class_body)
+) @definition.enum_class
+
+; Interface declarations
+(class_declaration
+  (type_identifier) @name.definition.interface
+) @definition.interface
+
+; Regular function declarations
 (function_declaration
   (simple_identifier) @name.definition.function
 ) @definition.function
 
+
+; Suspend function declarations
+(function_declaration
+  (modifiers
+    (function_modifier) @_modifier (#eq? @_modifier "suspend"))
+  (simple_identifier) @name.definition.suspend_function
+) @definition.suspend_function
+
+; Object declarations
 (object_declaration
   (type_identifier) @name.definition.object
 ) @definition.object
 
-(property_declaration
-  (simple_identifier) @name.definition.property
-) @definition.property
+; Companion object declarations
+(companion_object) @definition.companion_object
 
-(type_alias
-  (type_identifier) @name.definition.type
-) @definition.type
+
+
+; Annotation class declarations
+(class_declaration
+  (modifiers
+    (class_modifier) @_modifier (#eq? @_modifier "annotation"))
+  (type_identifier) @name.definition.annotation_class
+) @definition.annotation_class
 `
