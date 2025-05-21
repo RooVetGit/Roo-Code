3d818d27be8aa9c0cdabde5ebcf329222a07df82
cte <cestreich@gmail.com>
2025-05-20 23:36:14 -0700

    Typo

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -13,7 +13,7 @@ import { Equals, Keys, AssertEqual } from "../utils/type-fu"
 import { publisher, name, version } from "../../package.json"
 
 // These ENV variables can be defined by ESBuild when building the extension
-// in order to override the values in package.json. This allows us built
+// in order to override the values in package.json. This allows us to build
 // different extension variants with the same package.json file.
 // The build process still needs to emit a modified package.json for consumption
 // by VSCode, but that build artifact is not used during the transpile step of
