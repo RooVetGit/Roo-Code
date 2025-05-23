fcd8842b0027dce75568c831c9e3de8faf836d5b
cte <cestreich@gmail.com>
2025-05-23 00:49:01 -0700

    Add tests, remove debugging

--- webview-ui/src/components/settings/ApiOptions.tsx
+++ webview-ui/src/components/settings/ApiOptions.tsx
@@ -462,8 +462,6 @@ const ApiOptions = ({
 				modelInfo={selectedModelInfo}
 			/>
 
-			{/* <pre>{JSON.stringify(selectedModelInfo, null, 2)}</pre> */}
-
 			{!fromWelcomeView && (
 				<>
 					<DiffSettingsControl
