48857824dbbff4450f109608f8d0734b5632c69b
xyOz <xyoz-dev@proton.me>
2025-05-28 13:54:08 +0100

    Issue fixed.

--- src/core/task/Task.ts
+++ src/core/task/Task.ts
@@ -1096,11 +1096,15 @@ export class Task extends EventEmitter<ClineEvents> {
 			}),
 		)
 
+		const { showRooIgnoredFiles = true } = (await this.providerRef.deref()?.getState()) ?? {}
+
 		const parsedUserContent = await processUserContentMentions({
 			userContent,
 			cwd: this.cwd,
 			urlContentFetcher: this.urlContentFetcher,
 			fileContextTracker: this.fileContextTracker,
+			rooIgnoreController: this.rooIgnoreController,
+			showRooIgnoredFiles,
 		})
 
 		const environmentDetails = await getEnvironmentDetails(this, includeFileDetails)
