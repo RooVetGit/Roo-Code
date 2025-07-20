06e3aeb15e79457d676ac3ab904d481228005502
Felix Anhalt <fanhalt@oev.de>
2025-07-20 23:49:16 +0200

    feat: implement file-based editing option as experimental setting

--- src/integrations/editor/EditingProviderFactory.ts
+++ src/integrations/editor/EditingProviderFactory.ts
@@ -1,4 +1,3 @@
-import * as vscode from "vscode"
 import { DiffViewProvider } from "./DiffViewProvider"
 import { FileWriter } from "./FileWriter"
 import { IEditingProvider } from "./IEditingProvider"
